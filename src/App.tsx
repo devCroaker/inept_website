e6c7c0da9efb08a2297f19f1825ae4539df60edc
devCroaker <dev.croaker@gmail.com>
2021-08-06 20:23:27 -0700

    First main update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import { AppBar, Button, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@material-ui/core'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu'
+import HomeIcon from '@material-ui/icons/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [state, setState] = useState({
+    drawer: false
+  })
+
+  const toggleDrawer = (open: boolean) => {
+    setState({...state, drawer: open})
+  }
+
+  const list = () => (
+    <div style={{width: 250}}>
+      <List>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <ListItem button key="Home">
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List>
+        <Link to="/login" style={{ textDecoration: 'none' }}>
+          <ListItem button key="Login">
+            <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+            <ListItemText primary="Login" />
+          </ListItem>
+        </Link>
+      </List>
     </div>
+  )
+
+  return (
+    <Router>
+      <div style={{flexGrow: 1}}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              onClick={() => toggleDrawer(true)}
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" style={{flexGrow: 1}}>Inept Engineers</Typography>
+            <Link to="/login" style={{ textDecoration: 'none', color: 'white', float: 'right', marginRight: '25px' }}>
+              <Button color="inherit">Login</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </div>
+
+      <Drawer anchor="left" open={state.drawer} onClose={() => toggleDrawer(false)}>
+        {list()}
+      </Drawer>
+
+      <Switch>
+        <Route path="/login">
+          <h3 style={{marginLeft: '50px'}}>Also Under Construction</h3>
+        </Route>
+        <Route path="/">
+          <h2 style={{marginLeft: '50px'}}>Under Construction</h2>
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
