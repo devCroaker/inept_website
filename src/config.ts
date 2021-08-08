8287fd4a94cfa8999080646cfc02e7bf47d745ba
devCroaker <dev.croaker@gmail.com>
2021-08-08 16:50:38 -0700

    Add env variables to build command

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    cognito: {
+        REGION: process.env.REGION!,
+        USER_POOL_ID: process.env.USER_POOL_ID!,
+        APP_CLIENT_ID: process.env.APP_CLIENT_ID!,
+        IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID!
+    }
+}
